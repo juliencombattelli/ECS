a7413aefa30048e6a710a4fb432b907af3628b95
juliencombattelli <julien.combattelli@hotmail.com>
2017-07-31 17:54:13 +0200

    Add static integer sequence generator to easely iterate over tuple

--- src/entity.cpp
+++ src/entity.cpp
@@ -12,8 +12,11 @@
 #include <cassert>
 #include <map>
 #include <cstdint>
+#include <sstream>
 
 #include <mul/type_traits_helper.hpp>
+#include <mul/stdx/tuple.hpp>
+#include <mul/stdx/index_range.hpp>
 #include <ecs/Entity.hpp>
 
 struct Position
@@ -112,12 +115,12 @@ int main()
 
     return 0;
 }*/
-
+/*
 std::map<std::string,std::string> data{
 	{"name","me"},
 	{"health","10"}
 };
-
+*/
 template<typename T>
 struct pair
 {
@@ -127,16 +130,105 @@ struct pair
 
 std::tuple<pair<Name>,pair<Health>> t;
 
+
+namespace test2
+{
+namespace detail
+{
+    template<
+        typename Integer,
+        typename SequenceBase,
+        Integer Begin,
+        Integer Step,
+        bool IsIncreasing
+    >
+    struct integer_range_impl;
+
+    template<
+        typename Integer,
+        Integer... N,
+        Integer Begin,
+        Integer Step
+    >
+    struct integer_range_impl<Integer, std::integer_sequence<Integer, N...>, Begin, Step, true>
+    {
+        using type = std::integer_sequence<Integer, (N * Step + Begin)...>;
+    };
+
+    template<
+        typename Integer,
+        Integer... N,
+        Integer Begin,
+        Integer Step
+    >
+    struct integer_range_impl<Integer, std::integer_sequence<Integer, N...>, Begin, Step, false>
+    {
+        using type = std::integer_sequence<Integer, (Begin - N * Step)...>;
+    };
+}
+
+template<
+    typename Integer,
+    Integer Begin,
+    Integer End,
+    Integer Step
+>
+using make_integer_range = typename detail::integer_range_impl<
+    Integer,
+    std::make_integer_sequence<
+        Integer,
+        ((Begin < End ? End - Begin : Begin - End) - 1) / Step + 1
+    >,
+    Begin,
+    Step,
+    (Begin < End)
+>::type;
+
+template<std::size_t Begin, std::size_t End, std::size_t Step>
+using make_index_range = make_integer_range<std::size_t, Begin, End, Step>;
+
+}
+
+template<typename Tuple, std::size_t... Ind>
+void print(Tuple&& t, std::index_sequence<Ind...> d)
+{
+    ((std::cout << std::get<Ind>(t).name << ' ') , ...);
+}
+
 int main()
 {
-	std::get<pair<Name>>(t).name = "name";
+	std::get<pair<Name>>(t).name = "namesdcsdcsdcsdc";
 	std::get<pair<Health>>(t).name = "health";
 
-	if(data.begin()->first == std::get<1>(t).name)
+	//print_tuple_impl(t, 0 , ...,  12);
+
+	print(t, test2::make_index_range<0u, 2u, 1u>{});
+
+	/*for(auto i : mul::stdx::index_range(0,std::tuple_size<decltype(t)>::value))
 	{
-		std::remove_reference<decltype(std::get<1>(t))>::type::value_type c{std::stoi(data[std::get<1>(t).name])};
-		std::cout << typeid(decltype(c)).name() << " = " << c.value << std::endl;
-	}
+		auto& element = mul::stdx::get(t,i);
+		for(const auto& pair : data)
+		{
+			if(pair.first == element.name)
+			{
+				std::stringstream ss(data[element.name]);
+				typename std::remove_reference<decltype(element)>::type::value_type c;
+				ss >> c.value;
+				std::cout << typeid(decltype(c)).name() << " = " << c.value << std::endl;
+			}
+		}
+	}*/
+
+	/*auto f = [&data](auto t, int i)
+	{
+		if(data.begin()->first == std::get<i>(t).name)
+		{
+			typename std::remove_reference<decltype(std::get<i>(t))>::type::value_type c{std::stoi(data[std::get<i>(t).name])};
+			std::cout << typeid(decltype(c)).name() << " = " << c.value << std::endl;
+		}
+	};
+
+	detail::for_each(t, f, detail::gen_seq<1>());*/
 
 	std::tuple<Name,Health> tuple{{"me"},{10}};
 
